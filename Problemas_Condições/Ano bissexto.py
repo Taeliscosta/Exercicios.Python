d9afbb4717bcc92128c0e3ed51a3ef7682138f14
Taélis Costa <127879613+Taeliscosta@users.noreply.github.com>
2023-04-08 14:54:39 -0300

    Create Ano bissexto.py

---
+++ Problemas_Condições/Ano bissexto.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+
+
+ano = int(input('Digite um ano: ')) 
+if (ano%4 == 0): 
+  print('É um ano bissexto!') 
+else:
+  print('Não é um ano bissexto!') 
